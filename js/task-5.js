ff3bb6c51222815e46d2c4d30cffc738abef6fda
d1m2o3n4 <d1m2o3n4@gmail.com>
2024-03-29 11:35:29 +0200

    add

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+btnEl.addEventListener('click', event => {
+  const randonColor = getRandomHexColor();
+  document.body.style.backgroundColor = randonColor;
+  spanEl.textContent = randonColor;
+});
